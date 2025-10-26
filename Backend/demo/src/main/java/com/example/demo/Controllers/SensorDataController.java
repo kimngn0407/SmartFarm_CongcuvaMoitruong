14a53544943d3252481c777600ff981f58b9b909
Luu Hoang Phuong Nam <nam11072019@gmail.com>
2025-10-26 20:34:58 +0700

    SensorDataController

---
+++ Backend/demo/src/main/java/com/example/demo/Controllers/SensorDataController.java
@@ -0,0 +1,41 @@
+package com.example.demo.Controllers;
+
+import com.example.demo.DTO.SensorDataDTO;
+import com.example.demo.Entities.SensorDataEntity;
+import com.example.demo.Services.SensorDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sensor-data")
+public class SensorDataController {
+
+    @Autowired
+    private SensorDataService sensorDataService;
+
+    @PostMapping
+    public SensorDataDTO saveSensorData(@RequestBody SensorDataDTO dto) {
+        return sensorDataService.saveSensorData(dto);
+    }
+
+    @GetMapping
+    public List<SensorDataDTO> getSensorData(
+            @RequestParam Long sensorId,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime from,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to
+    ) {
+        return sensorDataService.getSensorData(sensorId, from, to);
+    }
+
+    @GetMapping("/latest/{sensorId}")
+    public ResponseEntity<List<SensorDataDTO>> getLatestSensorData(@PathVariable Long sensorId) {
+        List<SensorDataDTO> latestData = sensorDataService.getLatestDataBySensorId(sensorId);
+        return ResponseEntity.ok(latestData);
+    }
+
+}
