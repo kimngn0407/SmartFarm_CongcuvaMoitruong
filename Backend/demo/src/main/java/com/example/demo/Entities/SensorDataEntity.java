55561c24b8fcaa47db5a7a4de068e2cb4e8acbe6
Đặng Lê Hoàng <133183692+Lehoangf@users.noreply.github.com>
2025-10-27 18:22:03 +0700

    sensor entity

---
+++ Backend/demo/src/main/java/com/example/demo/Entities/SensorDataEntity.java
@@ -0,0 +1,24 @@
+package com.example.demo.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Sensor_data")
+@Data
+public class SensorDataEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sensor_id")
+    private SensorEntity sensor;
+
+    private Float value;
+
+    private LocalDateTime time;
+}
+
